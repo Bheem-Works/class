f96020ce9aec5643886aa1b92bd3417a5939a551
bheem-works <shokokomichan098@gmail.com>
2025-08-18 22:14:14 +0545

    que used for

---
+++ Qeue.js
@@ -0,0 +1,14 @@
+// What is queue ?
+// |-> A queue is a type of the data structure that store multiple collection of the items;
+// -> Add to the back of hte queue (enqueue);
+// -> it also remove the last of the queue(dequeue);
+// -> Referes to the queue to the fifo;
+
+// What queue actually use case for ? 
+// -> 1. For the fifo (firs in first out);
+// -> 2. Tracking request for the limited resources;
+// -> 3. Graph algorithm;
+
+class Queue {
+  constructor() {}
+}
---
+++ Stack.js
@@ -0,0 +1,50 @@
+// Link list by using the class;
+
+class StackNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    if (this.size === 0) {
+      this.top = new StackNode(val);
+    } else {
+      const pushedNode = new StackNode(val);
+      console.log(pushedNode, "pushedNode"); // output : {val:'b',next:null};
+
+      pushedNode.next = this.top;
+      console.log("pushed.next", pushedNode.next); // ouput : Stacknode {val:'a',next : null};
+
+      this.top = pushedNode;
+      console.log(this.top, "this.top"); // ouptu : {val:'b',next : StackNode{val:'a',next:null}}
+    }
+    this.size++;
+  }
+
+  pop() {
+    if (this.size === 0) return null;
+    const poppedNode = this.top;
+    console.log("poppedNodeValue", poppedNode);
+    this.top = this.top.next; // what about the ,next???
+    this.size--;
+    return poppedNode.val;
+  }
+
+  getTop() {
+    return this.top.val;
+  }
+}
+
+const linkList = new Stack();
+linkList.push("a");
+linkList.push("b");
+console.log(linkList.pop());
+console.log(linkList.size, "size");
