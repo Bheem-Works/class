58d51067beb5d5677132d3616a84ac17ca7c2da4
bheem-works <shokokomichan098@gmail.com>
2025-08-18 10:10:15 +0545

    Fourth commit and succesfully worked.

--- assDes.js
+++ assDes.js
@@ -1,32 +1,33 @@
-// Malai ya feri ascending ra descending mai chita bujheko chaina feri aile lai.
+// find out the number into the asscending order;
 class FindAscending {
-  constructor(){
-    this.num = [1,3,8,2,4];    
+  constructor(number) {
+    this.num = number;
+    console.log("number", number);
   }
-  // think about the right name. 
+
   manageInOdr() {
     console.log(this.num); // let's check it is it working or not?
-    for(let i = 0; i <= this.num.length; i++){
-      for(let j = i + 1; j<=this.num.length;j++){
+    for (let i = 0; i <= this.num.length; i++) {
+      for (let j = i + 1; j <= this.num.length; j++) {
         let temp;
-        if(this.num[i] > this.num[j]){
+        if (this.num[i] > this.num[j]) {
           temp = this.num[i];
-          this.num[i] = this.num[j]; 
+          this.num[i] = this.num[j];
           this.num[j] = temp;
         }
       }
     }
   }
 
-  print(){
-    for(let i = 0; i <= this.num.length; i++){
+  print() {
+    for (let i = 0; i <= this.num.length; i++) {
       console.log(this.num[i]);
     }
   }
 }
 
-const test = new FindAscending();
+const test = new FindAscending([1, 34, 2, 76, 4]);
 test.manageInOdr();
 test.print(); // output : 1,2,3,4....;
 
-// It work but i want this in the number form; 
+// It work but i want this in the number form;
