f3131b8d7cb6fa7f66c927be8d861d9f026b1838
bheem-works <shokokomichan098@gmail.com>
2025-08-22 01:53:06 +0545

    problemSolved

--- NumberIncreasing.js
+++ NumberIncreasing.js
@@ -6,5 +6,12 @@ and it should match the number;
 */
 
 function Match (n){
-    
+    if(n == 1) return true; 
+
+    for(let i = 0; i < n; i++){
+        if(i * 3 == n || i + 5 == n) return true; 
+    }
+    return false; 
 }; 
+
+console.log(Match(5)); // true
